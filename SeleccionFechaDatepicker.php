860caa72d5b3d2369585a6603c820f49a9af72d3
Kike <kike@areavisual.net>
2017-01-05 15:00:11 +0100

    Validación de fechas en JQuery y Javascript
    Utilizo dos versiones. Una jQuery para selección de fechas y otra
    versión en Javascript hecha con funciones.

--- SeleccionFechaDatepicker.php
+++ SeleccionFechaDatepicker.php
@@ -1,67 +1,100 @@
 <!DOCTYPE html>
 
-<!--
-<html>
-
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>PRIMEROS PASOS CON BRACKETS</title>
-        <meta name="description" content="Una guía interactiva de primeros pasos para Brackets.">
-        <link type="text/css" href="css/ui-darkness/jquery-ui-1.8.23.custom.css" rel="Stylesheet" />
-        <script type="text/javascript" src="js/jquery-1.8.0.min.js"></script>
-        <script type="text/javascript" src="js/jquery-ui-1.8.23.custom.min.js"></script>
-        <script type="text/javascript" src="js/jquery.ui.datepicker-es.js"></script>
-        
-        <script type="text/javascript">
-        $(function() {    
-            $('#demo9').datepicker({
-                onSelect: function(selectedDate) {
-                    var dataString = 'date='+selectedDate;
-                    $.ajax({
-                        type: "POST",
-                        url: "save.php",
-                        data: dataString,
-                        success: function(data) {
-                            $('#result').empty();
-                            $('#result').html(data);
-                        }
-                    });
-                }
-
-            });
-        });
-        </script>
-        
-    </head>
-    <body>
-        <label>Selecciona fecha:</label><input id="demo9" type="text" name="fecha2"><div id="result"></div>
-<input type="date" name="cumpleanios" step="1" min="1900-01-01" max="2040-12-31" value="<?php echo date("Y-m-d");?>" onclick="verfecha()">
-
-
-</body>
-</html>
--->
-<!doctype html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>jQuery UI Datepicker - Default functionality</title>
+  <title>jQuery UI Datepicker y Javascript</title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
+
+  <script type="text/javascript">
+  // Versión funciones en JQuery   
+      
   $( function() {
     $( "#datepicker" ).datepicker();
-  } );
+  } );    
+
+
+    
+  // Versión funciones en JavaScript  
+  
+  function validarFormatoFecha(campo) {
+      
+      var RegExPattern = /^\d{1,2}\/\d{1,2}\/\d{2,4}$/;
+      if ((campo.match(RegExPattern)) && (campo!='')) {
+            return true;
+      } else {
+            return false;
+      }
+  }
+
+  function existeFecha(fecha){
+      
+        var fechaf = fecha.split("/");
+        var day = fechaf[0];
+        var month = fechaf[1];
+        var year = fechaf[2];
+        var date = new Date(year,month,'0');
+        if((day-0)>(date.getDate()-0)){
+              return false;
+        }
+        return true;
+  }
+   
+  function existeFecha2 (fecha) {
+      
+          var fechaf = fecha.split("/");
+          var d = fechaf[0];
+          var m = fechaf[1];
+          var y = fechaf[2];
+          return m > 0 && m < 13 && y > 0 && y < 32768 && d > 0 && d <= (new Date(y, m, 0)).getDate();
+  }
+
+
+  function validarFechaMenorActual(date){
+      
+        var x=new Date();
+        var fecha = date.split("/");
+        x.setFullYear(fecha[2],fecha[1]-1,fecha[0]);
+        var today = new Date();
+   
+        if (x >= today)
+          return false;
+        else
+          return true;
+  }
+
+  function comprobar() {
+    
+    var fecha = document.getElementById('fechaX').value;
+
+    if(validarFormatoFecha(fecha)){
+          if(existeFecha(fecha)){
+                alert("La fecha introducida es correcta.");
+          }else{
+                alert("La fecha introducida no existe.");
+          }
+    }else{
+          alert("El formato de la fecha es incorrecta, por favor vuelva a escribirla tal como se le indica.");
+    }
+  }
+
   </script>
+
+  
 </head>
-<body>
- 
-<p>Date: <input type="text" id="datepicker" value="<?php echo date("Y-m-d");?>"></p>
- 
- 
-</body>
+
+    
+    <body>
+
+        <p>Seleccione la fecha: <input type="text" name="fecha" id="datepicker" value="<?php echo date("d/m/Y");?>" ></p>
+
+        <p>Introduzca la fecha: <input type="text" name="fechaX" id="fechaX" value="<?php echo date("d/m/Y");?>" onblur="comprobar()"></p> 
+
+
+    </body>
+    
 </html>
+
