936b0c5c124b355ec433999069b7991a60d19458
Kike <kike@areavisual.net>
2017-01-12 17:51:57 +0100

    Nueva función de validación
    Incorporo nueva función para validar la fecha.

--- SeleccionFechaDatepicker.php
+++ SeleccionFechaDatepicker.php
@@ -48,6 +48,7 @@
    
 
   // Validar si la fecha introducida es anterior o menor a la actual.
+      
   function validarFechaMenorActual(date){
       
         var x=new Date();
@@ -61,7 +62,7 @@
           return true;
   }
 
-  // Utilizo las dos funciones anteriores para comprobar que la fecha introducida es correcta
+  // Utilizo las dos funciones anteriores para comprobar que la fecha introducida es correcta.
       
   function comprobar() {
     
@@ -77,6 +78,85 @@
           alert("El formato de la fecha es incorrecta, por favor vuelva a escribirla tal como se le indica.");
     }
   }
+      
+      
+  // Nueva función de validación de fecha DD/MM/AAAA
+      
+    function validaFechaDDMMAAAA(){
+        
+        var fecha = document.getElementById('fechaY').value;
+        var dtCh= "/";
+        var minYear=1900;
+        var maxYear=2100;
+        function isInteger(s){
+            var i;
+            for (i = 0; i < s.length; i++){
+                var c = s.charAt(i);
+                if (((c < "0") || (c > "9"))) return false;
+            }
+            return true;
+        }
+        function stripCharsInBag(s, bag){
+            var i;
+            var returnString = "";
+            for (i = 0; i < s.length; i++){
+                var c = s.charAt(i);
+                if (bag.indexOf(c) == -1) returnString += c;
+            }
+            return returnString;
+        }
+        function daysInFebruary (year){
+            return (((year % 4 == 0) && ( (!(year % 100 == 0)) || (year % 400 == 0))) ? 29 : 28 );
+        }
+        function DaysArray(n) {
+            for (var i = 1; i <= n; i++) {
+                this[i] = 31
+                if (i==4 || i==6 || i==9 || i==11) {this[i] = 30}
+                if (i==2) {this[i] = 29}
+            }
+            return this
+        }
+        function isDate(dtStr){
+            var daysInMonth = DaysArray(12)
+            var pos1=dtStr.indexOf(dtCh)
+            var pos2=dtStr.indexOf(dtCh,pos1+1)
+            var strDay=dtStr.substring(0,pos1)
+            var strMonth=dtStr.substring(pos1+1,pos2)
+            var strYear=dtStr.substring(pos2+1)
+            strYr=strYear
+            if (strDay.charAt(0)=="0" && strDay.length>1) strDay=strDay.substring(1)
+            if (strMonth.charAt(0)=="0" && strMonth.length>1) strMonth=strMonth.substring(1)
+            for (var i = 1; i <= 3; i++) {
+                if (strYr.charAt(0)=="0" && strYr.length>1) strYr=strYr.substring(1)
+            }
+            month=parseInt(strMonth)
+            day=parseInt(strDay)
+            year=parseInt(strYr)
+            if (pos1==-1 || pos2==-1){
+                return false
+            }
+            if (strMonth.length<1 || month<1 || month>12){
+                return false
+            }
+            if (strDay.length<1 || day<1 || day>31 || (month==2 && day>daysInFebruary(year)) || day > daysInMonth[month]){
+                return false
+            }
+            if (strYear.length != 4 || year==0 || year<minYear || year>maxYear){
+                return false
+            }
+            if (dtStr.indexOf(dtCh,pos2+1)!=-1 || isInteger(stripCharsInBag(dtStr, dtCh))==false){
+                return false
+            }
+            return true
+        }
+        if(isDate(fecha)){
+            alert("La fecha introducida es correcta.");
+            return true;
+        }else{
+            alert("La fecha introducida no es correcta. Por favor escríbala como se le pide dd/mm/aaaa");
+            return false;
+        }
+    }
 
   </script>
 
@@ -85,12 +165,12 @@
 
     
     <body>
+        
+        <p>Seleccione la fecha ( Uso de función datepicker() ): <input type="text" name="fecha" id="datepicker" value="<?php echo date("d/m/Y");?>" ></p>
 
-        <p>Seleccione la fecha: <input type="text" name="fecha" id="datepicker" value="<?php echo date("d/m/Y");?>" ></p>
+        <p>Introduzca la fecha ( Uso de función comprobar() ): <input type="text" name="fechaX" id="fechaX" value="<?php echo date("d/m/Y");?>" onblur="comprobar()"></p> 
 
-        <p>Introduzca la fecha: <input type="text" name="fechaX" id="fechaX" value="<?php echo date("d/m/Y");?>" onblur="comprobar()"></p> 
-
-
+        <p>Introduzca la fecha ( Uso de función validaFechaDDMMAAAA() ): <input type="text" name="fechaY" id="fechaY" value="<?php echo date("d/m/Y");?>" onblur="return validaFechaDDMMAAAA()"></p>
     </body>
     
 </html>
